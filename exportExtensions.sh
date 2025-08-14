31735aa07fb3d5c1da16f0b7633dbea49bef70bc
Caleb Baird <79653447+calebbaird5@users.noreply.github.com>
2025-08-14 13:29:32 -0600

    update a bunch of stuff

---
+++ exportExtensions.sh
@@ -0,0 +1,2 @@
+# populate the extensions.txt file with the list of installed extensions
+code --list-extensions > extensions.txt
--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 cat extensions.txt | xargs -L 1 code --install-extension
 
+# install local extensions in the ./extensions directory
+for ext in ./extensions/*; do
+    if [ -d "$ext" ]; then
+        echo "Installing local extension: $ext"
+        code --install-extension "$ext"
+    fi
+done
+
 rm -f ~/Library/Application\ Support/Code/User/settings.json
 ln -s ./settings.json ~/Library/Application\ Support/Code/User/settings.json
 
